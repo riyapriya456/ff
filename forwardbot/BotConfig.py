eed834a1c92cf4d4ebcab83f39268f49fbe6b792
Devilriya <logesh.12a3@gmail.com>
2022-12-28 10:51:15 +0530

    Update BotConfig.py

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -1,10 +1,10 @@
 from os import environ
 class Config(object):
-    API_ID = environ.get("API_ID", None)
-    API_HASH = environ.get("API_HASH", None)
-    BOT_TOKEN = environ.get("BOT_TOKEN", None)
-    STRING_SESSION = environ.get("STRING", None)
-    SUDO_USERS = environ.get("SUDO_USERS", None)
+    API_ID = environ.get("API_ID", "19376711")
+    API_HASH = environ.get("API_HASH", "97d6b9d4999508a0c29d71c348d33351")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "5649345445:AAEALDSlFIFTAxJxpsdE3JzfIBySuTKEnjU")
+    STRING_SESSION = environ.get("STRING", "1BVtsOHsBu0xRiT62eWFl7QUGee_RPU9RbAAHBz5Vl9T9BeAiZJHbpo6cjgKt5OIGaG0QF0scAISV1LJKJnU7laRsdoF0Tw0Ler7-8BORU0Zafc4Lq6UezHk1WTUHV4Hbvx6i0ulaRHqBQoi4vSTwWZrWeMJ2MOX4_s7clrwOqAkIgJOBXfqd2gp8SuAT8cKxyiB04ktRv5Z873hiKbkTM6uz4cyc7btoJMC1bp1rFI5_Zac5qVGN2laCWwKlLCyx029NfXztwA71Uzntu9U3wvPeDboRJOPuSL-ucU02wT5OFZla4QhU87qhGzasuHlm-pwFLi6MImSPDcl9qt-ktUsCT_QO34I=")
+    SUDO_USERS = environ.get("SUDO_USERS", "5493968060")
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
     HELP_MSG = """
     The Commands in the bot are:
